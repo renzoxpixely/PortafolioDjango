a7ac89ac462d71f9b7d6f54fc77cc67b4604dc38
renzo <renzo_or@hotmail.com>
2018-12-23 13:02:05 -0500

    primer commit del portafolio en django usuario: superuser

---
+++ core/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, HttpResponse
+
+html_base = """
+<h1>Mi web personal</h1>
+<ul>
+    <li><a href="/about-me">Acerca de</a></li>
+    <li><a href="/portfolio">Portafolio</a></li>
+    <li><a href="/contact">Contacto</a></li>
+</ul>
+"""
+
+
+
+
+# Create your views here.
+def home(request):
+    return render(request, "core/home.html")
+
+def about(request):
+    return render(request, "core/about.html")
+
+def portfolio(request):
+    return render(request, "core/portfolio.html")
+
+def contact(request):
+    return render(request, "core/contact.html")
